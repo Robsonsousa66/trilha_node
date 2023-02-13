febc34a6b65960621268f6fee3b6ee04e4e4a6e9
Robson Sousa <robsonsousasousa66@gmail.com>
2023-02-13 15:05:28 -0300

    feat: create project_02

--- projeto_01/src/database.js
+++ projeto_01/src/database.js
@@ -34,4 +34,22 @@ export class DataBase {
     this.#persist()
     return data;
   }
+
+  delete(table, id){
+    const rowIndex = this.#database[table].findIndex(row => row.id === id);
+
+    if(rowIndex > -1){
+      this.#database[table].splice(rowIndex, 1);
+      this.#persist()
+    }
+  }
+
+  update(table, id, data){
+    const rowIndex = this.#database[table].findIndex(row => row.id === id);
+
+    if(rowIndex > -1){
+      this.#database[table][rowIndex] = {id, ...data};
+      this.#persist()
+    }
+  }
 }
